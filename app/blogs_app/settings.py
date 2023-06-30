537843752f71b09f0294e7e102276fd7f3fffdd1
johan castrillon <castrillon53d@gmail.com>
2023-06-29 19:51:52 -0500

    settings

--- app/blogs_app/settings.py
+++ app/blogs_app/settings.py
@@ -98,19 +98,14 @@ WSGI_APPLICATION = 'blogs_app.wsgi.application'
 
 DATABASES = {
    'default': {
-        # 'ENGINE': os.getenv("DB_ENGINE"),
-        # 'NAME': os.getenv("DB_NAME"), 
-        # 'USER': os.getenv("DB_USER"),
-        # 'PASSWORD': os.getenv("DB_PASSWORD"),
-        # 'HOST': os.getenv("DB_HOST"), 
-        # 'PORT': os.getenv("DB_PORT"),
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"), 
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"), 
+        'PORT': os.getenv("DB_PORT")
 
-        'DB_ENGINE':"django.db.backends.postgresql_psycopg2",
-        'DB_HOST':"db.oqejzqvvyfdsuzemxwku.supabase.co",
-        'DB_NAME':"postgres",
-        'DB_USER':"postgres",
-        'DB_PASSWORD':"libero125libero12",
-        'DB_PORT':"5432"
+
     } 
 }
 
